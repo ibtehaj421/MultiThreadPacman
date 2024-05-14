f286e5a12eab4ee442f631757abe4d6e1e80990a
ibtehaj421 <114730186+ibtehaj421@users.noreply.github.com>
2024-05-14 19:19:46 +0500

    Update setup.h
    added new variables and used them in syncs

--- setup.h
+++ setup.h
@@ -3,7 +3,7 @@
 #include<pthread.h>
 #include <semaphore.h>
 #include <SFML/Graphics.hpp>
-
+#include<SFML/Audio.hpp>
 using namespace sf;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 sf::RenderWindow window(sf::VideoMode(500,600), "PACMAN");
@@ -20,33 +20,30 @@ pthread_attr_t unlinker;
 bool allThreadKiller = false;
 Clock pacClock;
 int readCount = 0; //initially set the read count to zero to basically restrict the write lock mechanism.
-sem_t reader;
+sem_t reader; //full
 sem_t reader2;
-
 sem_t reader3;
-sem_t reader4;
-sem_t reader5;
-sem_t reader6;
 
-
-sem_t readCounter;
+int readCounter; //allow upto 3 threads for now.
 Text score;
 Text pointNum;
 Font retro;
 int points;
-
+int sheetcol = 0;
 struct pellet
 {
     Sprite coin;
     bool status = true; //initially set as true as each pellet can be spawned and eaten will be set to false once a pellet gets eaten until re spawning of pellets.
 };
 struct pellet pellets[30][25];
-
-
+sem_t pacWrite; //empty
+int value;
 sem_t keyPermit, exitPermit; 
 Clock c1,c2,c3,c4;   //For each ghost
 Sprite ghost1,ghost2,ghost3,ghost4;
 Texture g1,g2,g3,g4;
 std::string g2Movement = "U";  //The random moving ghosts alwasy move up first
 std::string g3Movement = "U";
-pthread_mutex_t lock2 = PTHREAD_MUTEX_INITIALIZER;
+Music base;
+Music chomp;
+sem_t mute;
